f95a2b16e3773fbb2b3a1bf9eb21554d2c0c9801
NamitS27 <namitshah2711@gmail.com>
2022-08-13 17:11:07 +0530

    added dynamic heartbeat from multiple clients feature

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import redis
 from dateutil.relativedelta import relativedelta as rd
 import os
 from dotenv import load_dotenv
+from threading import Thread
+import time
+import heapq
 
 load_dotenv()
 
@@ -30,7 +33,9 @@ and expiry mechanism to the key-value pair in place.
 
 
 app = Flask(__name__)
-socket_ = SocketIO(app, async_mode='eventlet')
+socket_ = SocketIO(app, async_mode='gevent')
+
+client_queue = []
 
 
 @app.route('/')
@@ -62,6 +67,7 @@ def disconnect():
     # calculate the number of clients connected to the server
     num_clients = int(r.get('num_clients')) - 1
     r.set('num_clients', num_clients) # set the latest value of connected clients
+    # last step would be to remove the client from the client queue as well as from the redis
 
 
 @socket_.on('server_time')
@@ -89,7 +95,7 @@ def num_clients(id):
 def format_timespan(seconds):
     intervals = ['days', 'hours', 'minutes', 'seconds']
     readable_time = rd(seconds=seconds)
-    return ' '.join('{} {}'.format(getattr(readable_time, k), k) for k in intervals if getattr(readable_time, k))
+    return ' '.join('{} {}'.format(getattr(readable_time, k), k if getattr(readable_time,k) > 1 else k[:len(k)-1]) for k in intervals if getattr(readable_time, k))
 
 
 @socket_.on('client_time')
@@ -109,22 +115,44 @@ def ping(id):
     socket_.emit('ping', 'Connected', room=id)
 
 
-'''
-The following code can be uncommented in order to send a specific message to all clients after a specific time interval.
-The only disadvantage of this approach is that the message will be tried to sent to all clients, even if they are
-not connected to the server. Also, a thread will be created & will be consume CPU resources.
+def heartbeat_handler(socket_):
+    '''
+    Handles the dynamic heartbeat of multiple clients.
+    Based on the priority of the client whose heartbeat is the earliest, it will be the one to be removed from the queue.
+    and accordinly the heartbeat will be sent to the client.
 
-from threading import Thread
-def ping():
-    # A function which sends a ping to all clients every 60 seconds
+    This can also be achieved by using RabbitMQ asynchronous messaging (kinda serving like a webhook).
+    '''
     while True:
-        print('Sending ping to all clients...')
-        socket_.emit('ping', 'Connected', broadcast=True)
-        socket_.sleep(60)
+        if len(client_queue) > 0:
+            curr_job = heapq.heappop(client_queue)
+            if curr_job[0] > datetime.now().timestamp():
+                heapq.heappush(client_queue, curr_job)
+                time.sleep(curr_job[0] - datetime.now().timestamp())
+            else:
+                t = Thread(target=lambda: socket_.emit('ping', 'Connected', room=curr_job[1]))
+                t.start()
+                heapq.heappush(client_queue, (datetime.now().timestamp() + curr_job[2], curr_job[1], curr_job[2]))
 
-t = Thread(target=ping)
-t.start()
-'''
+
+hbt_thread = Thread(target=heartbeat_handler, args=(socket_,))
+hbt_thread.start()
+
+
+@socket_.on('heartbeat')
+def heartbeat(data):
+    socket_id = data['socket_id']
+    heartbt = int(data['heartbeat'])
+    job = (datetime.now().timestamp() + heartbt, socket_id, heartbt)
+    heapq.heappush(client_queue, job)
+    # The following code is for the condition when there are suppose 4 clients connected to the server.
+    # The least of them has a heartbeat of lets say 1 hour and a new client request to have a heartbeat of 5 minutes,
+    # then we will have to wait for 1 hour and then send the ping to the new client. Hence for the same, we can
+    # call the interrupt for the thread which will ultimately start running again and we can send the ping to the new client.
+    # As python does not have a mechanism to interrupt a thread, we can use the following psuedocode to do the same.
+    # if (job == client_queue[0]):
+    #     hbt_thread.interrupt()
+
 
 if __name__ == '__main__':
     socket_.run(app, port=os.getenv('PORT') or 8000, debug=os.getenv('DEBUG') == 'True')
